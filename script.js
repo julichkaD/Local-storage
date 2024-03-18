165ee798cca8e58e8bc90a63cfe2003b92aa75a7
Julia Darwish <darwishjulia4@gmail.com>
2024-03-18 14:43:03 -0400

    Updated script file

--- script.js
+++ script.js
@@ -10,7 +10,34 @@ function addItem(e) {
     done: false,
   };
   items.push(item);
+  populateList(items, itemsList);
   this.reset();
 }
+ function populateList(plates = [], platesList) {
+   platesList.innerHTML = plates
+     .map((plate, i) => {
+       return `
+        <li>
+          <input type="checkbox" data-index=${i} id="item${i}" ${
+         plate.done ? "checked" : ""
+       } />
+          <label for="item${i}">${plate.text}</label>
+        </li>
+      `;
+     })
+     .join("");
+ }
 
-addItems.addEventListener("submit", addItem);
+ function toggleDone(e) {
+   if (!e.target.matches("input")) return; // skip this unless it's an input
+   const el = e.target;
+   const index = el.dataset.index;
+   items[index].done = !items[index].done;
+   localStorage.setItem("items", JSON.stringify(items));
+   populateList(items, itemsList);
+ }
+
+ addItems.addEventListener("submit", addItem);
+ itemsList.addEventListener("click", toggleDone);
+
+ populateList(items, itemsList);
